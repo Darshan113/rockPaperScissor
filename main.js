d4890e22d9ac93098f23487c39b0faf7b221d08c
Darshan Panchal <dppanchal106@gmail.com>
2023-10-18 20:19:07 +0530

    feat : complete logic and dom

--- main.js
+++ main.js
@@ -51,22 +51,59 @@
 
 // game()
 
+const roundResult = document.querySelector('#roundresult')
+const FinalResult = document.querySelector('#finalResult')
+const player = document.querySelector('#player span')
+const computer = document.querySelector('#computer span')
 
 
+    let playerScore = 0 , compScore = 0
     const choices = ['rock','paper','scissor']
     const getComputerChoice = (choices) => {
        let compSelection = Math.floor(Math.random() * choices.length)
        return choices[compSelection]
     }
 
+        const determineResult = (playerSelection , compSelection) => {
+        if(playerSelection == compSelection){
+            roundResult.textContent = "It\' TIE"
+        }else if( (playerSelection == "rock" && compSelection == "scissor") || 
+                  (playerSelection == "scissor" && compSelection == "paper") ||
+                  (playerSelection == "paper" && compSelection == "rock")
+        ){
+            playerScore += 1
+            roundResult.textContent = `You Win : ${playerSelection} beats ${compSelection}`
+        }else {
+            compScore += 1
+            roundResult.textContent =  `You Lose : ${compSelection} beats ${playerSelection}`
+        }
+      player.textContent = playerScore
+      computer.textContent = compScore
+      if(playerScore == 5 || compScore == 5){
+            if(playerScore == 5)
+                FinalResult.textContent = "🎉🎉 YOU WON 🎉🎉"
+            else
+            FinalResult.textContent = "😤😤 YOU LOSE 😤😤"
+        }
+      
+    }
+
 const playRound = (btn) =>{
+    // get choices
    const playerChoice = btn.textContent.toLowerCase()
    const computerChoice = getComputerChoice(choices).toLowerCase()
-   console.log(playerChoice)
-   console.log(computerChoice)
+   determineResult(playerChoice,computerChoice)
 
 }
 
+const restart = () =>{
+    roundResult.textContent =  ``
+    FinalResult.textContent = ''
+    playerScore = 0
+    compScore = 0
+    player.textContent = '0'
+    computer.textContent = '0'
+}
 
 
 
