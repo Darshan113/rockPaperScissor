a26c59d7ac32990ea151f26f2a6ced51ea140dfb
Darshan Panchal <dppanchal106@gmail.com>
2023-10-18 19:16:32 +0530

    feat : handle event

--- main.js
+++ main.js
@@ -1,52 +1,72 @@
 
 
 
-const game = () => {
+// const game = () => {
+//     const choices = ['rock','paper','scissor']
+//     const getComputerChoice = (choices) => {
+//        let compSelection = Math.floor(Math.random() * choices.length)
+//        return choices[compSelection]
+//     }
+//     const determineResult = (playerSelection , compSelection) => {
+//         if(playerSelection == compSelection){
+//             return 'It\'s TIE'
+//         }else if( (playerSelection == "rock" && compSelection == "scissor") || 
+//                   (playerSelection == "scissor" && compSelection == "paper") ||
+//                   (playerSelection == "paper" && compSelection == "rock")
+//         ){
+//             playerScore += 1
+//             return `You Win : ${playerSelection} beats ${compSelection}`
+//         }else {
+//             compScore += 1
+//             return `You Lose : ${compSelection} beats ${playerSelection}`
+//         }
+//     }
+//     let playerScore = 0 , compScore = 0
+
+//     const playRound = (playerSelection,compSelection) =>{
+//         playerSelection = playerSelection.toLowerCase();
+//         compSelection = compSelection.toLowerCase()
+//         return determineResult(playerSelection,compSelection)
+//     }
+    
+    
+//     while(1){
+//         if(playerScore == 5 || compScore == 5){
+//             if(playerScore == 5)
+//                console.log('you win this whole game') 
+//             else
+//               console.log('you lose this whole game')
+//             break
+//         }else{
+//             let playerSelection = prompt('you select : (select from rock , paper , scissor) ')
+//             let compSelection = getComputerChoice(choices)
+//             console.log(playRound(
+//                 playerSelection , compSelection
+//             ))
+//             console.log(`player score : ${playerScore}  AND comp score ${compScore}`)
+//         }
+//     }
+
+// }
+
+// game()
+
+
+
     const choices = ['rock','paper','scissor']
     const getComputerChoice = (choices) => {
        let compSelection = Math.floor(Math.random() * choices.length)
        return choices[compSelection]
     }
-    const determineResult = (playerSelection , compSelection) => {
-        if(playerSelection == compSelection){
-            return 'It\'s TIE'
-        }else if( (playerSelection == "rock" && compSelection == "scissor") || 
-                  (playerSelection == "scissor" && compSelection == "paper") ||
-                  (playerSelection == "paper" && compSelection == "rock")
-        ){
-            playerScore += 1
-            return `You Win : ${playerSelection} beats ${compSelection}`
-        }else {
-            compScore += 1
-            return `You Lose : ${compSelection} beats ${playerSelection}`
-        }
-    }
-    let playerScore = 0 , compScore = 0
 
-    const playRound = (playerSelection,compSelection) =>{
-        playerSelection = playerSelection.toLowerCase();
-        compSelection = compSelection.toLowerCase()
-        return determineResult(playerSelection,compSelection)
-    }
-    
-    
-    while(1){
-        if(playerScore == 5 || compScore == 5){
-            if(playerScore == 5)
-               console.log('you win this whole game') 
-            else
-              console.log('you lose this whole game')
-            break
-        }else{
-            let playerSelection = prompt('you select : (select from rock , paper , scissor) ')
-            let compSelection = getComputerChoice(choices)
-            console.log(playRound(
-                playerSelection , compSelection
-            ))
-            console.log(`player score : ${playerScore}  AND comp score ${compScore}`)
-        }
-    }
+const playRound = (btn) =>{
+   const playerChoice = btn.textContent.toLowerCase()
+   const computerChoice = getComputerChoice(choices).toLowerCase()
+   console.log(playerChoice)
+   console.log(computerChoice)
 
 }
 
-game()
+
+
+
